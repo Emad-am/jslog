9504744766ca7dc0dca2ecc02018ce1a7286ea7a
Emad Amiripour <emadamiripour307@gmail.com>
2024-10-30 10:15:32 +0330

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,9 @@ import LogLevel from "./models/enum/loglevel";
 import Environment from "./models/enum/enviroment";
 
 
-function init(host: string, secret: string, platform_name: string): Logger {
+function init(host: string, enviroment: Environment, secret: string, platform_name: string): Logger {
     
-    return new Logger(host, secret, platform_name);
+    return new Logger(host, secret, platform_name, enviroment);
 }
 
 export { init, Log, LogLevel, Environment };
