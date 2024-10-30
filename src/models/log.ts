9a27350c5c2e69f35b55c46c03432bf48fe010c4
Emad Amiripour <emadamiripour307@gmail.com>
2024-10-30 10:15:30 +0330

    Update log.ts

--- src/models/log.ts
+++ src/models/log.ts
@@ -1,8 +1,6 @@
-import Environment from "./enum/enviroment";
 import LogLevel from "./enum/loglevel";
 class Log {
   constructor(
-    public enviroment: Environment,
     public level: LogLevel,
     public title: string,
     public message: string
